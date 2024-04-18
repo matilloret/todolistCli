d529243f835037960ad81f7bf2e8490b83982b96
matilloret <mati_lloret@hotmail.com>
2024-04-18 18:55:24 -0300

    añadiendo funcionalidad complete

--- todos.go
+++ todos.go
@@ -1,6 +1,9 @@
 package todos
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 // la funcionalidad que va a tener es representar a las tareas
 
@@ -14,6 +17,30 @@ type Item struct {
 // El usuario tiene la capacidad de crear varias tareas lo que deberia hacer es crear un arreglo
 type Todos []Item
 
-func (t *Todos) Add() {
+func (t *Todos) Add(titutlo string) {
+	todo := Item{
+		Titulo:       titulo,
+		Done:         false,
+		Created_at:   time.Now(),
+		Completed_at: time.Time{},
+	}
 
+	*t = append(*t, todo)
+}
+
+// el usuario pueda ser capaz de marcar como completada una tarea
+
+func (t *Todos) Complete(index int) error {
+	// crear una instancia de la estructura todos
+	ls := *t
+	// manejo de derror por si el usuario ingresa un numero  invalido
+	if index <= 0 || index > len(ls) {
+		return errors.New("Index invalido")
+	}
+
+	// Actualizar la tarea
+	ls[index-1].Completed_at = time.Now()
+	ls[index-1].Done = true
+
+	return nil
 }
